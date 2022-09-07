f319413f29c387ed52ee8272ab8e982c6ea69d68
CooBoo84 <kennie_svensson@hotmail.com>
2022-09-07 10:53:21 +0200

    feat: add tracker

---
+++ src/components/DataTitle.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="text-center">
+        <h2 class="text-3xl font-bold">{{ text }}</h2>
+        <div class="text-2xl mt-4 mb-10">{{ timestamp }}</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'DataTitle',
+    props: ['text', 'dataDate'],
+    computed: {
+        timestamp: function() {
+            const date = new Date();
+            const n = date.toDateString();
+            const time = date.toLocaleTimeString();
+
+            return n + ' ' + time
+        }
+    }
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
